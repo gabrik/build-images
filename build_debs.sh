743c8b70b363c5ae3d7c32b5408c621f1c23b356
gabrik <gabriele.baldoni@gmail.com>
2020-02-24 16:36:36 +0100

    updated debian making scripts
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- build_debs.sh
+++ build_debs.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -e
 
-dpkg -b fog05_0.1-1
-dpkg -b fog05-plugin-net-linuxbridge_0.1-1
-dpkg -b fog05-plugin-fdu-native_0.1-1
-dpkg -b fog05-plugin-fdu-lxd_0.1-1
-dpkg -b fog05-plugin-fdu-kvm_0.1-1
-dpkg -b libzenoh-dev_0.0.1-1
+cd fog05-plugin-os-linux && ./generate_deb.sh
 
 mkdir debs
 mv *.deb debs/
