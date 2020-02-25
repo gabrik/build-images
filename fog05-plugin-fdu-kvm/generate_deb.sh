a5bd2d37fdb561e3d1c22ab81e1da18bbb4e9fc7
gabrik <gabriele.baldoni@gmail.com>
2020-02-25 18:53:34 +0100

    updated scripts to use version, branch and image

--- fog05-plugin-fdu-kvm/generate_deb.sh
+++ fog05-plugin-fdu-kvm/generate_deb.sh
@@ -5,8 +5,9 @@ set -e
 UBUNTU="ubuntu:bionic"
 DEBIAN="debian:10-slim"
 
-docker pull $DEBIAN
-docker run -it -d --name build $DEBIAN bash
+docker pull ${IMAGE}
+docker run -it -d --name build ${IMAGE} bash
+
 # deps
 docker exec build apt update
 docker exec build apt install build-essential devscripts lintian dh-make git wget jq python3 python3-dev python3-pip unzip cmake sudo -y
@@ -17,19 +18,20 @@ docker exec build bash -c "cd /root/ && git clone https://github.com/atolab/yaks
 docker exec build bash -c "cd /root/ && git clone https://github.com/eclipse-fog05/sdk-python -b 0.1 --depth 1 && cd sdk-python && make && make install"
 docker exec build bash -c "cd /root/ && git clone https://github.com/eclipse-fog05/api-python -b 0.1 --depth 1 && cd api-python && make install"
 # building deb file
-docker exec build bash -c "cd /root/ && git clone https://github.com/gabrik/plugin-fdu-kvm"
-docker exec build bash -c "mkdir /root/build && cd /root && cp -r plugin-fdu-kvm build/fog05-plugin-fdu-kvm-0.1 && cd build/fog05-plugin-fdu-kvm-0.1 && rm -rf .git && make clean && cd .. && tar -czvf fog05-plugin-fdu-kvm-0.1.tar.gz fog05-plugin-fdu-kvm-0.1"
-docker exec build bash -c "export DEBEMAIL=\"info@adlink-labs.tech\" && export DEBFULLNAME=\"ADLINK Technology Inc\" && cd /root/build/fog05-plugin-fdu-kvm-0.1 && dh_make -f ../fog05-plugin-fdu-kvm-0.1.tar.gz -s -y"
-docker exec build bash -c 'cd /root/build/fog05-plugin-fdu-kvm-0.1 && printf "override_dh_auto_install:\n\tmkdir -p \$\$(pwd)/debian/fog05-plugin-fdu-kvm/lib/systemd/system/\n\t\$(MAKE) KVM_PLUGIN_DIR=\$\$(pwd)/debian/fog05-plugin-fdu-kvm/etc/fos/plugins/plugin-fdu-kvm SYSTEMD_DIR=\$\$(pwd)/debian/fog05-plugin-fdu-kvm/lib/systemd/system/ install">> debian/rules'
+docker exec build bash -c "cd /root/ && git clone https://github.com/gabrik/plugin-fdu-kvm -b ${BRANCH} --depth 1"
+docker exec build bash -c "mkdir /root/build && cd /root && cp -r plugin-fdu-kvm build/fog05-plugin-fdu-kvm-${VERSION} && cd build/fog05-plugin-fdu-kvm-${VERSION} && rm -rf .git && make clean && cd .. && tar -czvf fog05-plugin-fdu-kvm-${VERSION}.tar.gz fog05-plugin-fdu-kvm-${VERSION}"
+docker exec build bash -c "export DEBEMAIL=\"info@adlink-labs.tech\" && export DEBFULLNAME=\"ADLINK Technology Inc\" && cd /root/build/fog05-plugin-fdu-kvm-${VERSION} && dh_make -f ../fog05-plugin-fdu-kvm-${VERSION}.tar.gz -s -y"
+docker exec build -e VERSION=${VERSION} bash -c 'cd /root/build/fog05-plugin-fdu-kvm-${VERSION} && printf "override_dh_auto_install:\n\tmkdir -p \$\$(pwd)/debian/fog05-plugin-fdu-kvm/lib/systemd/system/\n\t\$(MAKE) KVM_PLUGIN_DIR=\$\$(pwd)/debian/fog05-plugin-fdu-kvm/etc/fos/plugins/plugin-fdu-kvm SYSTEMD_DIR=\$\$(pwd)/debian/fog05-plugin-fdu-kvm/lib/systemd/system/ install">> debian/rules'
 
-docker cp templates/changelog build:/root/build/fog05-plugin-fdu-kvm-0.1/debian/changelog
-docker cp templates/postinst build:/root/build/fog05-plugin-fdu-kvm-0.1/debian/postinst
-docker cp templates/postrm build:/root/build/fog05-plugin-fdu-kvm-0.1/debian/postrm
-docker cp templates/control build:/root/build/fog05-plugin-fdu-kvm-0.1/debian/control
-docker cp templates/copyright build:/root/build/fog05-plugin-fdu-kvm-0.1/debian/copyright
+sed -i "s/FOSVERSION/${VERSION}/g" templates/changelog
+docker cp templates/changelog build:/root/build/fog05-plugin-fdu-kvm-${VERSION}/debian/changelog
+docker cp templates/postinst build:/root/build/fog05-plugin-fdu-kvm-${VERSION}/debian/postinst
+docker cp templates/postrm build:/root/build/fog05-plugin-fdu-kvm-${VERSION}/debian/postrm
+docker cp templates/control build:/root/build/fog05-plugin-fdu-kvm-${VERSION}/debian/control
+docker cp templates/copyright build:/root/build/fog05-plugin-fdu-kvm-${VERSION}/debian/copyright
 
-docker exec build bash -c "cd /root/build/fog05-plugin-fdu-kvm-0.1 && debuild --preserve-envvar PATH -us -uc  && ls -l ../"
-docker exec build bash -c "cd /root/build/ && dpkg -I fog05-plugin-fdu-kvm_0.1-1_amd64.deb"
-docker cp build:/root/build/fog05-plugin-fdu-kvm_0.1-1_amd64.deb ../fog05-plugin-fdu-kvm_0.1-1_amd64_debian_buster.deb
+docker exec build bash -c "cd /root/build/fog05-plugin-fdu-kvm-${VERSION} && debuild --preserve-envvar PATH -us -uc  && ls -l ../"
+docker exec build bash -c "cd /root/build/ && dpkg -I fog05-plugin-fdu-kvm_${VERSION}-1_amd64.deb"
+docker cp build:/root/build/fog05-plugin-fdu-kvm_${VERSION}-1_amd64.deb ../fog05-plugin-fdu-kvm_${VERSION}-1_amd64_${IMAGE}.deb
 
 docker container rm --force build
--- fog05-plugin-net-linuxbridge/generate_deb.sh
+++ fog05-plugin-net-linuxbridge/generate_deb.sh
@@ -17,17 +17,19 @@ docker exec build bash -c "cd /root/ && git clone https://github.com/atolab/yaks
 docker exec build bash -c "cd /root/ && git clone https://github.com/eclipse-fog05/sdk-python -b 0.1 --depth 1 && cd sdk-python && make && make install"
 docker exec build bash -c "cd /root/ && git clone https://github.com/eclipse-fog05/api-python -b 0.1 --depth 1 && cd api-python && make install"
 # building deb file
-docker exec build bash -c "cd /root/ && git clone https://github.com/gabrik/plugin-net-linuxbridge"
-docker exec build bash -c "mkdir /root/build && cd /root && cp -r plugin-net-linuxbridge build/fog05-plugin-net-linuxbridge-0.1 && cd build/fog05-plugin-net-linuxbridge-0.1 && rm -rf .git && make clean && cd .. && tar -czvf fog05-plugin-net-linuxbridge-0.1.tar.gz fog05-plugin-net-linuxbridge-0.1"
-docker exec build bash -c "export DEBEMAIL=\"info@adlink-labs.tech\" && export DEBFULLNAME=\"ADLINK Technology Inc\" && cd /root/build/fog05-plugin-net-linuxbridge-0.1 && dh_make -f ../fog05-plugin-net-linuxbridge-0.1.tar.gz -s -y"
-docker exec build bash -c 'cd /root/build/fog05-plugin-net-linuxbridge-0.1 && printf "override_dh_auto_install:\n\tmkdir -p \$\$(pwd)/debian/fog05-plugin-net-linuxbridge/lib/systemd/system/\n\tmkdir -p \$\$(pwd)/debian/fog05-plugin-net-linuxbridge/usr/bin\n\t\$(MAKE) LB_PLUGIN_DIR=\$\$(pwd)/debian/fog05-plugin-net-linuxbridge/etc/fos/plugins/plugin-net-linuxbridge SYSTEMD_DIR=\$\$(pwd)/debian/fog05-plugin-net-linuxbridge/lib/systemd/system/ BIN_DIR=\$\$(pwd)/debian/fog05-plugin-net-linuxbridge/usr/bin install">> debian/rules'
-docker cp templates/changelog build:/root/build/fog05-plugin-net-linuxbridge-0.1/debian/changelog
-docker cp templates/postinst build:/root/build/fog05-plugin-net-linuxbridge-0.1/debian/postinst
-docker cp templates/control build:/root/build/fog05-plugin-net-linuxbridge-0.1/debian/control
-docker cp templates/copyright build:/root/build/fog05-plugin-net-linuxbridge-0.1/debian/copyright
+docker exec build bash -c "cd /root/ && git clone https://github.com/gabrik/plugin-net-linuxbridge  -b ${BRANCH} --depth 1"
+docker exec build bash -c "mkdir /root/build && cd /root && cp -r plugin-net-linuxbridge build/fog05-plugin-net-linuxbridge-${VERSION} && cd build/fog05-plugin-net-linuxbridge-${VERSION} && rm -rf .git && make clean && cd .. && tar -czvf fog05-plugin-net-linuxbridge-${VERSION}.tar.gz fog05-plugin-net-linuxbridge-${VERSION}"
+docker exec build bash -c "export DEBEMAIL=\"info@adlink-labs.tech\" && export DEBFULLNAME=\"ADLINK Technology Inc\" && cd /root/build/fog05-plugin-net-linuxbridge-${VERSION} && dh_make -f ../fog05-plugin-net-linuxbridge-${VERSION}.tar.gz -s -y"
+docker exec -e VERSION=${VERSION} build bash -c 'cd /root/build/fog05-plugin-net-linuxbridge-${VERSION} && printf "override_dh_auto_install:\n\tmkdir -p \$\$(pwd)/debian/fog05-plugin-net-linuxbridge/lib/systemd/system/\n\tmkdir -p \$\$(pwd)/debian/fog05-plugin-net-linuxbridge/usr/bin\n\t\$(MAKE) LB_PLUGIN_DIR=\$\$(pwd)/debian/fog05-plugin-net-linuxbridge/etc/fos/plugins/plugin-net-linuxbridge SYSTEMD_DIR=\$\$(pwd)/debian/fog05-plugin-net-linuxbridge/lib/systemd/system/ BIN_DIR=\$\$(pwd)/debian/fog05-plugin-net-linuxbridge/usr/bin install">> debian/rules'
 
-docker exec build bash -c "cd /root/build/fog05-plugin-net-linuxbridge-0.1 && debuild --preserve-envvar PATH -us -uc  && ls -l ../"
-docker exec build bash -c "cd /root/build/ && dpkg -I fog05-plugin-net-linuxbridge_0.1-1_amd64.deb"
-docker cp build:/root/build/fog05-plugin-net-linuxbridge_0.1-1_amd64.deb ../fog05-plugin-net-linuxbridge_0.1-1_amd64_debian_buster.deb
+sed -i "s/FOSVERSION/${VERSION}/g" templates/changelog
+docker cp templates/changelog build:/root/build/fog05-plugin-net-linuxbridge-${VERSION}/debian/changelog
+docker cp templates/postinst build:/root/build/fog05-plugin-net-linuxbridge-${VERSION}/debian/postinst
+docker cp templates/control build:/root/build/fog05-plugin-net-linuxbridge-${VERSION}/debian/control
+docker cp templates/copyright build:/root/build/fog05-plugin-net-linuxbridge-${VERSION}/debian/copyright
+
+docker exec build bash -c "cd /root/build/fog05-plugin-net-linuxbridge-${VERSION} && debuild --preserve-envvar PATH -us -uc  && ls -l ../"
+docker exec build bash -c "cd /root/build/ && dpkg -I fog05-plugin-net-linuxbridge_${VERSION}-1_amd64.deb"
+docker cp build:/root/build/fog05-plugin-net-linuxbridge_${VERSION}-1_amd64.deb ../fog05-plugin-net-linuxbridge_${VERSION}-1_amd64_${IMAGE}.deb
 
 docker container rm --force build
