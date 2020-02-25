9562e21b1eed61028964ae256a9a1d129aec73fd
gabrik <gabriele.baldoni@gmail.com>
2020-02-25 15:22:22 +0100

    fix in python3-api debian generation
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- fog05-python3-api/generate_deb.sh
+++ fog05-python3-api/generate_deb.sh
@@ -15,12 +15,10 @@ docker exec build bash -c "cd /root/ && git clone https://github.com/atolab/zeno
 docker exec build bash -c "cd /root/ && git clone https://github.com/atolab/zenoh-python -b 0.3.0 --depth 1 && cd zenoh-python && python3 setup.py install"
 docker exec build bash -c "cd /root/ && git clone https://github.com/atolab/yaks-python -b 0.3.0 --depth 1 && cd yaks-python && make install"
 docker exec build bash -c "cd /root/ && git clone https://github.com/gabrik/api-python fog05-api-0.1"
-# build package
-docker exec build bash -c "cd /root/fog05-api-0.1 && make"
 #build deb
 docker exec build bash -c "cd /root && mkdir build && tar -czvf build/fog05-api-0.1.tar.gz fog05-api-0.1"
 docker exec build bash -c "cd /root/build && py2dsc fog05-api-0.1.tar.gz"
-docker exec build bash -c "cd /root/build/deb_dist/fog05-api-0.1.0 && dpkg-buildpackage -rfakeroot -uc -us"
+docker exec build bash -c "cd /root/build/deb_dist/fog05-0.1.0 && dpkg-buildpackage -rfakeroot -uc -us"
 docker exec build bash -c "cd /root/build/deb_dist/ && dpkg -I python3-fog05_0.1.0-1_all.deb"
 docker cp build:/root/build/deb_dist/python3-fog05_0.1.0-1_all.deb ../python3-fog05_0.1.0-1_all_debian_buster.deb
 
