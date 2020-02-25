d06a6befe4c152cc88c522c87b5e0175307bd19a
gabrik <gabriele.baldoni@gmail.com>
2020-02-25 10:44:16 +0100

    added linuxbridge plugin build
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- build_debs.sh
+++ build_debs.sh
@@ -3,6 +3,7 @@ set -e
 
 cd fog05-plugin-os-linux && ./generate_deb.sh && cd ..
 cd fog05 && ./generate_deb.sh && cd ..
+cd fog05-plugin-net-linuxbridge && ./generate_deb.sh && cd ..
 
 mkdir debs
 mv *.deb debs/
