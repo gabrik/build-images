99ec4df4165d02b644930d7e1630748d944c4554
gabrik <gabriele.baldoni@gmail.com>
2020-02-25 11:49:45 +0100

    added native fdu plugin and fixes in linuxbridge
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- build_debs.sh
+++ build_debs.sh
@@ -2,8 +2,10 @@
 set -e
 
 cd fog05-plugin-os-linux && ./generate_deb.sh && cd ..
-cd fog05 && ./generate_deb.sh && cd ..
 cd fog05-plugin-net-linuxbridge && ./generate_deb.sh && cd ..
+cd fog05-plugin-fdu-native && ./generate_deb.sh && cd ..
+cd fog05 && ./generate_deb.sh && cd ..
+
 
 mkdir debs
 mv *.deb debs/
