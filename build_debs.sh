d8fd9b5fa541de9e10d483ea62d7aae92dfd85d8
gabrik <gabriele.baldoni@gmail.com>
2020-02-25 13:12:34 +0100

    Added lxd plugin and env variable to decide which package to build, parallel jobs
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- build_debs.sh
+++ build_debs.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 set -e
 
-cd fog05-plugin-os-linux && ./generate_deb.sh && cd ..
-cd fog05-plugin-net-linuxbridge && ./generate_deb.sh && cd ..
-cd fog05-plugin-fdu-native && ./generate_deb.sh && cd ..
-cd fog05-plugin-fdu-kvm && ./generate_deb.sh && cd ..
-cd fog05 && ./generate_deb.sh && cd ..
-
-
-mkdir debs
-mv *.deb debs/
+case "$PKG" in
+    fog05)
+        cd fog05
+        ./generate_deb.sh
+        ;;
+    fog05-plugin-os-linux)
+        cd fog05-plugin-os-linux
+        ./generate_deb.sh
+        ;;
+    fog05-plugin-net-linuxbridge)
+        cd fog05-plugin-net-linuxbridge
+        ./generate_deb.sh
+        ;;
+    fog05-plugin-fdu-native)
+        cd fog05-plugin-fdu-native
+        ./generate_deb.sh
+        ;;
+    fog05-plugin-fdu-kvm)
+        cd fog05-plugin-fdu-kvm
+        ./generate_deb.sh
+        ;;
+    fog05-plugin-fdu-lxd)
+        cd fog05-plugin-fdu-lxd
+        ./generate_deb.sh
 
 exit 0
