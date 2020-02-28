f8d68f582afe8edc5cd544dbc5e658eaeb1304d2
gabrik <gabriele.baldoni@gmail.com>
2020-02-28 17:27:22 +0100

    building python api and sdk from branch 0.1.x
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- fog05-plugin-net-linuxbridge/generate_deb.sh
+++ fog05-plugin-net-linuxbridge/generate_deb.sh
@@ -14,8 +14,8 @@ docker exec build pip3 install pyangbind sphinx
 docker exec build bash -c "cd /root/ && git clone https://github.com/atolab/zenoh-c -b 0.3.0 --depth 1 && cd zenoh-c && make && make install"
 docker exec build bash -c "cd /root/ && git clone https://github.com/atolab/zenoh-python -b 0.3.0 --depth 1 && cd zenoh-python && python3 setup.py install"
 docker exec build bash -c "cd /root/ && git clone https://github.com/atolab/yaks-python -b 0.3.0 --depth 1 && cd yaks-python && make install"
-docker exec build bash -c "cd /root/ && git clone https://github.com/eclipse-fog05/sdk-python -b v0.1.0 --depth 1 && cd sdk-python && make && make install"
-docker exec build bash -c "cd /root/ && git clone https://github.com/eclipse-fog05/api-python -b v0.1.0 --depth 1 && cd api-python && make install"
+docker exec build bash -c "cd /root/ && git clone https://github.com/eclipse-fog05/sdk-python 0.1.x --depth 1 && cd sdk-python && make && make install"
+docker exec build bash -c "cd /root/ && git clone https://github.com/eclipse-fog05/api-python 0.1.x --depth 1 && cd api-python && make install"
 # building deb file
 docker exec build bash -c "cd /root/ && git clone https://github.com/eclipse-fog05/plugin-net-linuxbridge  -b ${BRANCH} --depth 1"
 docker exec build bash -c "mkdir /root/build && cd /root && cp -r plugin-net-linuxbridge build/fog05-plugin-net-linuxbridge-${VERSION} && cd build/fog05-plugin-net-linuxbridge-${VERSION} && rm -rf .git && make clean && cd .. && tar -czvf fog05-plugin-net-linuxbridge-${VERSION}.tar.gz fog05-plugin-net-linuxbridge-${VERSION}"
