988be946f6da31750e1b00df9ce49031bc4174d2
gabrik <gabriele.baldoni@gmail.com>
2020-07-22 10:50:45 +0200

    added containerd build and using already baked container image for agent
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

---
+++ containerd/generate_deb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+
+git clone https://github.com/docker/containerd-packaging
+
+cd containerd-packaging
+
+./scripts/new-deb-release ${VERSION}
+
+cp ../templates/containerd.service ./common/containerd.service
+
+make REF=${BRANCH} BUILD_IMAGE=docker.io/library/${IMAGE}
+
+dpkg -I build/ubuntu/bionic/arm64/containerd.io_${VERSION}-1_arm64.deb
+
+cp  build/ubuntu/bionic/arm64/containerd.io_${VERSION}-1_arm64.deb ../
+
+rm -rf containerd-packaging
