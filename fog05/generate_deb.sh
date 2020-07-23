6af6ebd1c029fba97162354d0eae27dfdf43cc4b
gabrik <gabriele.baldoni@gmail.com>
2020-07-23 18:29:52 +0200

    added deploy of artifact for agent
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- fog05/generate_deb.sh
+++ fog05/generate_deb.sh
@@ -38,3 +38,7 @@ docker cp build-agent:/root/fog05-${VERSION}.tar.gz ../fog05-${VERSION}.tar.gz
 
 
 docker container rm --force build-agent
+
+echo $KEY > key
+scp -i ./key ../fog05_${VERSION}-1_amd64.deb $USER@$SERVER:~
+rm key
