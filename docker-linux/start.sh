dbf15e890a5de8063b5b4cdaf3da42da088afbb7
gabrik <gabriele.baldoni@gmail.com>
2021-02-02 13:56:07 +0100

    update scripts
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- docker-linux/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CONF=/etc/fos/plugins/plugin-os-linux/linux_plugin.json
-
-sh -c "echo $FOG_NODE_ID | xargs -i  jq  '.configuration.nodeid = \"{}\"' $CONF > /tmp/agent.tmp && mv /tmp/agent.tmp $CONF"
-sh -c "echo tcp/$ZENOH_IP_ADDRESS:7447 | xargs -i  jq  '.configuration.ylocator = \"{}\"' $CONF > /tmp/agent.tmp && mv /tmp/agent.tmp $CONF"
-
-/etc/fos/plugins/plugin-os-linux/linux_plugin /etc/fos/plugins/plugin-os-linux/linux_plugin.json
--- fog05/generate_deb.sh
+++ fog05/generate_deb.sh
@@ -10,47 +10,34 @@ docker run -it -d --name build-agent ${IMAGE} bash
 
 # install deps
 docker exec build-agent apt update
-docker exec build-agent apt install build-essential devscripts lintian dh-make git wget jq libev-dev libssl-dev m4 pkg-config rsync unzip cmake sudo -y
-# install opam
-docker exec build-agent wget -O opam https://github.com/ocaml/opam/releases/download/2.0.6/opam-2.0.6-x86_64-linux
-docker exec build-agent install ./opam /usr/local/bin/opam
-docker exec build-agent opam init --compiler=4.09.0 --disable-sandboxing
-# install other deps
-docker exec build-agent bash -c "eval \$(opam env) && opam install dune.1.11.4 atdgen.2.0.0 conf-libev ocp-ocamlres websocket-lwt.2.12 -y"
-docker exec build-agent bash -c "eval \$(opam env) && opam pin add apero-core https://github.com/atolab/apero-core.git#0.4.6 -y"
-docker exec build-agent bash -c "eval \$(opam env) && opam pin add dynload-sys https://github.com/atolab/apero-core.git#0.4.6 -y"
-docker exec build-agent bash -c "eval \$(opam env) && opam pin add apero-net https://github.com/atolab/apero-net.git#0.4.6 -y"
-docker exec build-agent bash -c "eval \$(opam env) && opam pin add apero-time https://github.com/atolab/apero-time.git#0.4.6 -y"
-docker exec build-agent bash -c "eval \$(opam env) && opam pin add zenoh-proto https://github.com/atolab/zenoh.git#0.3.0 -y"
-docker exec build-agent bash -c "eval \$(opam env) && opam pin add zenoh-ocaml https://github.com/atolab/zenoh.git#0.3.0 -y"
-docker exec build-agent bash -c "eval \$(opam env) && opam pin add yaks-common https://github.com/atolab/yaks-common.git#0.3.0 -y"
-docker exec build-agent bash -c "eval \$(opam env) && opam pin add yaks-ocaml https://github.com/atolab/yaks-ocaml.git#0.3.0 -y"
-docker exec build-agent bash -c "eval \$(opam env) && opam pin add fos-sdk https://github.com/eclipse-fog05/sdk-ocaml.git#${BRANCH} -y"
-docker exec build-agent bash -c "eval \$(opam env) && opam pin add fos-fim-api https://github.com/eclipse-fog05/api-ocaml.git#${BRANCH}  -y"
+docker exec build-agent apt install build-essential devscripts debhelper pkg-config libssl-dev curl -y
+# install rust nightly
+docker exec build-agent curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs -o /tmp/rust.sh
+docker exec build-agent chmod +x /tmp/rust.sh
+docker exec build-agent /tmp/rust.sh --default-toolchain nightly -y
+# install cargo deb
+docker exec build-agent bash -c "cargo install cargo-deb"
+
 # clone repo
-docker exec build-agent bash -c "cd /root && git clone https://github.com/eclipse-fog05/agent -b ${BRANCH} --depth 1"
-# building a debian package
-docker exec build-agent bash -c "eval \$(opam env) && mkdir /root/build && cd /root && cp -r agent build/fog05-${VERSION} && cd build/fog05-${VERSION} && rm -rf .git && make clean && cd .. && tar -czvf fog05-${VERSION}.tar.gz fog05-${VERSION}"
-docker exec build-agent bash -c "eval \$(opam env) && cd /root/build/fog05-${VERSION} && make clean"
-docker exec build-agent bash -c "eval \$(opam env) && export DEBEMAIL=\"info@adlink-labs.tech\" && export DEBFULLNAME=\"ADLINK Technology Inc.\" && cd /root/build/fog05-${VERSION} && dh_make -f ../fog05-${VERSION}.tar.gz -s -y"
-docker exec -e VERSION=${VERSION} build-agent bash -c 'cd /root/build/fog05-${VERSION} && printf "override_dh_auto_install:\n\tmkdir -p \$\$(pwd)/debian/fog05/lib/systemd/system/\n\t\$(MAKE) FOS_DIR=\$\$(pwd)/debian/fog05/etc/fos SYSTEMD_DIR=\$\$(pwd)/debian/fog05/lib/systemd/system/ install\n">> debian/rules'
+docker exec build-agent bash -c "cd /root && git clone https://github.com/eclipse-fog05/fog05 -b ${BRANCH} --depth 1"
+# build
+docker exec build-agent bash -c "cd /root/fog05/ && cargo check"
+docker exec build-agent bash -c "cd /root/fog05/ && cargo build --release"
 
-sed -i "s/FOSVERSION/${VERSION}/g" templates/changelog
-docker cp templates/changelog build-agent:/root/build/fog05-${VERSION}/debian/changelog
-docker cp templates/postinst build-agent:/root/build/fog05-${VERSION}/debian/postinst
-docker cp templates/postrm build-agent:/root/build/fog05-${VERSION}/debian/postrm
-docker cp templates/control build-agent:/root/build/fog05-${VERSION}/debian/control
-docker cp templates/copyright build-agent:/root/build/fog05-${VERSION}/debian/copyright
+# debian packages
+docker exec build-agent bash -c "cd /root/fog05/ && cargo deb -p fog05-agent --no-build"
+docker exec build-agent bash -c "cd /root/fog05/ && cargo deb -p fog05-fosctl --no-build"
 
-docker exec build-agent bash -c "eval \$(opam env) && cd /root/build/fog05-${VERSION} && debuild --preserve-envvar PATH -us -uc  && ls -l ../"
-docker exec build-agent bash -c "cd /root/build/ && dpkg -I fog05_${VERSION}-1_amd64.deb"
+# check packages
+docker exec build-agent bash -c 'cd /root/fog05/ && dpkg -I ./target/release/debian/fog05-agent_$(cat fog05-agent/Cargo.toml | grep version | head -n1 | sed 's/[^"]*"\([^"]*\)".*/\1/' | tr "-" "~")_amd64.deb'
+docker exec build-agent bash -c 'cd /root/fog05/ && dpkg -I ./target/release/debian/fog05-fosctl_$(cat fog05-agent/Cargo.toml | grep version | head -n1 | sed 's/[^"]*"\([^"]*\)".*/\1/' | tr "-" "~")_amd64.deb'
 
 
 
-docker cp build-agent:/root/build/fog05_${VERSION}-1_amd64.deb ../fog05_${VERSION}-1_amd64_${IMAGE}.deb
+# docker cp build-agent:/root/build/fog05_${VERSION}-1_amd64.deb ../fog05_${VERSION}-1_amd64_${IMAGE}.deb
 
-docker exec build-agent bash -c "eval \$(opam env) && cd /root/agent && make && cd .. && tar -czvf fog05-${VERSION}.tar.gz agent"
-docker cp build-agent:/root/fog05-${VERSION}.tar.gz ../fog05-${VERSION}.tar.gz
+# docker exec build-agent bash -c "eval \$(opam env) && cd /root/agent && make && cd .. && tar -czvf fog05-${VERSION}.tar.gz agent"
+# docker cp build-agent:/root/fog05-${VERSION}.tar.gz ../fog05-${VERSION}.tar.gz
 
 
 docker container rm --force build-agent
