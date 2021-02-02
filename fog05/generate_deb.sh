ccdcc67cd67c5e1492add711f8a72ef56e0c3649
gabrik <gabriele.baldoni@gmail.com>
2021-02-02 15:09:40 +0100

    fix in generate_deb.sh
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- fog05/generate_deb.sh
+++ fog05/generate_deb.sh
@@ -19,7 +19,7 @@ docker exec build-agent /tmp/rust.sh --default-toolchain nightly -y
 docker exec build-agent bash -c 'source $HOME/.cargo/env && cargo install cargo-deb'
 
 # clone repo
-docker exec build-agent bash -c 'source $HOME/.cargo/env && cd /root && git clone https://github.com/eclipse-fog05/fog05 -b ${BRANCH} --depth 1'
+docker exec -e BRANCH=$BRANCH build-agent bash -c 'source $HOME/.cargo/env && cd /root && git clone https://github.com/eclipse-fog05/fog05 -b $BRANCH'
 # build
 docker exec build-agent bash -c 'source $HOME/.cargo/env && cd /root/fog05/ && cargo check'
 docker exec build-agent bash -c 'source $HOME/.cargo/env && cd /root/fog05/ && cargo build --release'
@@ -30,7 +30,7 @@ docker exec build-agent bash -c 'source $HOME/.cargo/env && cd /root/fog05/ && c
 
 # check packages
 docker exec build-agent bash -c "cd /root/fog05/ && dpkg -I ./target/release/debian/fog05-agent_$(cat fog05-agent/Cargo.toml | grep version | head -n1 | sed 's/[^"]*"\([^"]*\)".*/\1/' | tr '-' '~')_amd64.deb"
-docker exec build-agent bash -c "cd /root/fog05/ && dpkg -I ./target/release/debian/fog05-fosctl_$(cat fog05-agent/Cargo.toml | grep version | head -n1 | sed 's/[^"]*"\([^"]*\)".*/\1/' | tr '-' '~')_amd64.deb"
+docker exec build-agent bash -c "cd /root/fog05/ && dpkg -I ./target/release/debian/fog05-fosctl_$(cat fog05-fosctl/Cargo.toml | grep version | head -n1 | sed 's/[^"]*"\([^"]*\)".*/\1/' | tr '-' '~')_amd64.deb"
 
 
 
